176ef8725ebef5ef44a871bd664374922c9f29f5
Max Goldstein <maxgoldstein1@gmail.com>
2016-03-05 11:54:54 -0500

    Run elm-format

--- tests/test.elm
+++ tests/test.elm
@@ -1,34 +1,55 @@
 {- A simple test an example of the library.
-   Does not test every option, you can submit PRs for that.
+Does not test every option, you can submit PRs for that.
 -}
+
+
+module Main (..) where
 
 import Date
 import List exposing (map)
 import Date.Format
 import ElmTest exposing (test, Test, suite, assertEqual, elementRunner)
 
+
 -- test name, expected value, format string
-type alias TestTriple = (String, String, String)
+
+
+type alias TestTriple =
+  ( String, String, String )
+
 
 testData : List TestTriple
-testData = [ ("numeric date", "12/08/2014","%d/%m/%Y")
-           , ("spelled out date", "Tuesday, August 12, 2014","%A, %B %d, %Y")
-           , ("time", "04:53:51 AM","%I:%M:%S %p")
-           ]
+testData =
+  [ ( "numeric date", "12/08/2014", "%d/%m/%Y" )
+  , ( "spelled out date", "Tuesday, August 12, 2014", "%A, %B %d, %Y" )
+  , ( "time", "04:53:51 AM", "%I:%M:%S %p" )
+  ]
+
 
 sampleDate : Date.Date
-sampleDate = Date.fromTime 1407833631116.0
+sampleDate =
+  Date.fromTime 1.407833631116e12
+
 
 formatSampleDate : String -> String
-formatSampleDate fstring = Date.Format.format fstring sampleDate
+formatSampleDate fstring =
+  Date.Format.format fstring sampleDate
+
 
 formatTest : TestTriple -> TestTriple
-formatTest (a, b, format) = (a, b, formatSampleDate format)
+formatTest ( a, b, format ) =
+  ( a, b, formatSampleDate format )
+
 
 makeTest : TestTriple -> Test
-makeTest (described, expected, actual) = test described <| assertEqual expected actual
+makeTest ( described, expected, actual ) =
+  test described <| assertEqual expected actual
+
 
 tests : Test
-tests = suite "Formatting" <| map (makeTest << formatTest) testData
+tests =
+  suite "Formatting" <| map (makeTest << formatTest) testData
 
-main = elementRunner tests
+
+main =
+  elementRunner tests
