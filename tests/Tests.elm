e278269f65e85cbb3ef1ea0ba52c2f7acb246a79
Julian Jelfs <jjelfs@travelrepublic.co.uk>
2016-11-15 11:27:10 +0000

    made tests more resilient to timezone

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,7 +6,10 @@ module Tests exposing (all)
 
 import Date
 import Date.Format
+import String exposing (padLeft, join)
+import Result
 import Expect
+import Debug exposing (log)
 import Test exposing (..)
 
 
@@ -15,8 +18,8 @@ import Test exposing (..)
 
 all : Test
 all =
-    describe "Date Format tests" <|
-        List.map (makeTest << formatTest) testData
+    describe "Date Format tests"
+        <| List.map (makeTest << formatTest) testData
 
 
 type alias TestTriple =
@@ -27,15 +30,59 @@ testData : List TestTriple
 testData =
     [ ( "numeric date", "12/08/2014", "%d/%m/%Y" )
     , ( "spelled out date", "Tuesday, August 12, 2014", "%A, %B %d, %Y" )
-    , ( "time", "04:53:51 AM", "%I:%M:%S %p" )
-    , ( "time no spaces", "045351", "%H%M%S" )
-    , ( "literal %", "04%53", "%H%%%M" )
+    , ( "time", expectedTime, "%I:%M:%S %p" )
+    , ( "time no spaces", expectedTimeNoSpace, "%H%M%S" )
+    , ( "literal %", expectedTimeWithLiteral, "%H%%%M" )
     ]
+
+
+expectedTimeWithLiteral =
+    join "%" [ sampleHour, sampleMinute ]
+
+
+expectedTimeNoSpace =
+    join "" [ sampleHour, sampleMinute, sampleMinute ]
+
+
+expectedTime =
+    (join ":" [ sampleHour, sampleMinute, sampleMinute ])
+        ++ (case Date.hour sampleDate < 12 of
+                True ->
+                    " AM"
+
+                False ->
+                    " PM"
+           )
 
 
 sampleDate : Date.Date
 sampleDate =
-    Date.fromTime 1.407833631116e12
+    "2014-08-12T04:53:53Z"
+        |> Date.fromString
+        |> Result.withDefault (Date.fromTime 1.407833631116e12)
+
+
+pad : Int -> String
+pad =
+    toString >> (padLeft 2 '0')
+
+
+sampleHour : String
+sampleHour =
+    Date.hour sampleDate
+        |> pad
+
+
+sampleMinute : String
+sampleMinute =
+    Date.minute sampleDate
+        |> pad
+
+
+sampleSecond : String
+sampleSecond =
+    Date.second sampleDate
+        |> pad
 
 
 formatSampleDate : String -> String
@@ -50,4 +97,4 @@ formatTest ( a, b, format ) =
 
 makeTest : TestTriple -> Test
 makeTest ( described, expected, actual ) =
-    test described <| \() -> Expect.equal actual expected
+    test described <| \() -> Expect.equal (log "actual: " actual) (log "expected: " expected)
